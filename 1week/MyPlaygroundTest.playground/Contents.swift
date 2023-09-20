8f5356c2ac82b54e956c7666e02820b7ee57beb1
jinsejin <j03298969@gmail.com>
2023-09-20 17:20:44 +0900

    push 1weeks hw

---
+++ 1week/MyPlaygroundTest.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+
+func check(){
+    var fruit = "apple"
+    if (fruit == "apple") || (fruit == "banana") || (fruit == "cherry") {
+        print("\(fruit) 이다");
+    } else{
+        print("없음")
+    }
+
+}
--- 1week/example/example/ViewController.swift
+++ 1week/example/example/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let name : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "sejin"
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        configuration()
     }
-
-
+    private func configuration(){
+        view.addSubview(name)
+        let nameConstrain = [
+            name.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            name.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            name.heightAnchor.constraint(equalToConstant: 70),
+            name.widthAnchor.constraint(equalToConstant: 150),
+        ]
+        NSLayoutConstraint.activate(nameConstrain)
+    }
 }
 
